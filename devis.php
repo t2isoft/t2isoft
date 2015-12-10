494dda60894962e526fe7444575621232e75cbec
karl92390 <karl.kelleoglu@laposte.net>
2015-12-10 20:02:51 +0100

    add tarifs to website

---
+++ devis.php
@@ -0,0 +1,237 @@
+<?php include("header.php"); ?>             
+        <div class="black-background">
+            <div class="container">
+                 <!--header slider-->
+                   <div class="header-title box_orange">
+                        <div class="header-title-content text-left ">
+                            <h1 class="text-white">Tarifs création d'un site internet </h1>
+                        </div>
+                        
+                        
+                    </div>
+                    <!--/header slider-->
+            </div>
+        </div>
+        
+<!-- middle -->
+		<!-- middle -->
+		<div class="white-background">
+			<div class="container ">
+				<div id="middle" class="full_width box_white">
+					
+					<!-- content -->
+					<div class="content" role="main">
+						
+						<div class="title">
+							<h1 class="alignleft">Contactez-nous pour un devis sur mesure </h1>
+							<span class="btn button_link_medium btn_gray alignright">06 59 98 44 86</span>
+							<div class="clear"></div>
+						</div>
+						
+        <article class="post-detail">            
+            <div class="entry container">
+            
+                <div class="pricing_box row">
+                    <ul>
+                        <li class="price_col col col_1_5">
+                            <div class="price_item">
+                                <div class="price_col_head">
+                                    <h4>Services</h4>
+                                    <img src="images/money.png" alt="">
+                                </div>
+                                <div class="price_col_body">
+                                    <ul>
+                                        <li class="box_light_gray">Création du site</li>
+                                        <li>Panel d'administration</li>
+                                        <li class="box_light_gray">Installation sur hébergement web</li>
+                                        <li>Création hébergement web & nom de domaine</li>
+                                        <li class="box_light_gray">Optimisations du référencement naturel</li>
+                                        <li>Option e-commerce</li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </li>
+                        
+                        <li class="price_col col col_1_5">
+                            <div class="price_item">
+                                <div class="price_col_head">
+                                    <h3 class="text-bold">Simple</h3>
+                                    <h2>200€ HT</h2>
+                                </div>
+                                <div class="price_col_body">
+                                     <ul>
+                                        <li class="box_light_gray"><img src="images/ok.png" alt="Service inclus"></li>
+                                        <li><img src="images/ok.png" alt="Service inclus"></li>
+                                        <li style="padding: 20px 0px;" class="box_light_gray"><img src="images/ok.png" alt="Service inclus"></li>
+                                        <li style="padding: 20px 0px;"><img src="images/false.png" alt="Service non inclus"></li>
+                                        <li style="padding: 20px 0px;" class="box_light_gray"><img src="images/false.png" alt="Service non inclus"></li>
+                                        <li><img src="images/false.png" alt="Service non inclus"></li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </li>
+                        
+                        <li class="price_col big col col_1_5">
+                            <div class="price_item">
+                                <div class="price_col_head">
+                                     <div class="price_col_head">
+                                    <h3 class="text-bold">Clé en main</h3>
+                                    <h2>250€ HT</span></h2>
+                                </div>
+                                </div>
+                                <div class="price_col_body">
+                                    <ul>
+                                        <li class="bg_dark_orange"><img src="images/ok_red.png" alt="Service inclus"></li>
+                                        <li class="bg_light_orange"><img src="images/ok_red.png" alt="Service inclus"></li>
+                                        <li style="padding: 20px 0px;" class="bg_dark_orange"><img src="images/ok_red.png" alt="Service inclus"></li>
+                                        <li style="padding: 20px 0px;" class="bg_light_orange"><img src="images/ok_red.png" alt="Service inclus"></li>
+                                        <li style="padding: 20px 0px;" class="bg_dark_orange"><img src="images/false_red.png" alt="Service non inclus"></li>
+                                        <li class="bg_light_orange"><img src="images/false_red.png" alt="Service non inclus"></li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </li>
+                        
+                        <li class="price_col col col_1_5">
+                           <div class="price_item">
+                                <div class="price_col_head">
+                                    <h3 class="text-bold">Référencement</h3>
+                                    <h2>300€ HT</span></h2>
+                                </div>
+                                <div class="price_col_body">
+                                     <ul>
+                                        <li class="box_light_gray"><img src="images/ok.png" alt="Service inclus"></li>
+                                        <li><img src="images/ok.png" alt="Service inclus"></li>
+                                        <li style="padding: 20px 0px;" class="box_light_gray"><img src="images/ok.png" alt="Service inclus"></li>
+                                        <li style="padding: 20px 0px;"><img src="images/ok.png" alt="Service inclus"></li>
+                                        <li style="padding: 20px 0px;" class="box_light_gray"><img src="images/ok.png" alt="Service inclus"></li>
+                                        <li><img src="images/false.png" alt="Service non inclus"></li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </li>
+                        
+                        <li class="price_col col col_1_5">
+                            <div class="price_item">
+                                <div class="price_col_head">
+                                    <h3 class="text-bold">E-commerce</h3>
+                                    <h2>550€ HT</h2>
+                                </div>
+                                <div class="price_col_body">
+                                     <ul>
+                                        <li class="box_light_gray"><img src="images/ok.png" alt="Service inclus"></li>
+                                        <li><img src="images/ok.png" alt="Service inclus"></li>
+                                        <li style="padding: 20px 0px;" class="box_light_gray"><img src="images/ok.png" alt="Service inclus"></li>
+                                        <li style="padding: 20px 0px;"><img src="images/ok.png" alt="Service inclus"></li>
+                                        <li style="padding: 20px 0px;" class="box_light_gray"><img src="images/ok.png" alt="Service inclus"></li>
+                                        <li><img src="images/ok.png" alt="Service inclus"></li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </li>
+                    </ul>
+                    <div class="clear"></div>
+                </div>
+                <div class="row">
+                    <div class="col col_1_3">
+                        <div class="inner">
+                            <div class="widget-content widget_text">
+                            <h3 class="widget-title">Pourquoi nous choisir ?</h3>
+                            
+                                <div class="textwidget">
+                                    <p style="text-align: justify;">
+                                        Au delà du travail de <span class="text-orange">qualité</span> que nous vous fournissons, travailler avec nous c'est la <span class="text-orange">garantie</span> d'aboutir sur un <span class="text-orange">projet qui correspond à vos besoin</span>. <span class="text-orange">Aucun</span> forfait de maintenance surprise ou autres <span class="text-orange">frais cachés</span>.
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>    
+                    
+                    <div class="col col_1_3">
+                        <div class="inner">
+                            <div class="widget-content widget_text">
+                            <h3 class="widget-title">Méthodes de paiement</h3>
+                            
+                                <div class="textwidget">
+                                    <p style="text-aligne: justify;">
+                                        Nous acceptons les <span class="text-orange">virements bancaires</span> et les virements par <span class="text-orange">Paypal</span>.
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div> 
+
+                    <div class="col col_1_3">
+                        <div class="inner">
+                            <div class="widget-content faq_list">
+                            <h3 class="widget-title">Questions/réponses</h3>
+                                
+                                <div class="faq_question toggle"><span class="faq_q">Q:</span> <span class="faq_title">Pouvez-vous vous déplacez ?</span> <span class="ico"></span></div>
+                                <div class="faq_answer toggle_content">
+                                <p>Nous nous déplaçons en Île-De-France gratuitement pour tout projet facturé plus de 790€</p>
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>  
+                    
+                </div>
+               
+            </div>
+        </article>
+				<div class="clear"></div>
+			</div>
+			<div class="clear"></div>
+		</div>    
+	</div>
+            <!--/content-->
+           
+            <div class="clear"></div>
+        </div>
+         <div class="clear"></div>
+    </div>
+                <div class="footer-slider">
+                <div class="container">
+                    <div class="slider-inner">
+                        <h2 class="text-bold">Technologies utilisées</h2>
+                                    
+                           <div class="tf-footer-carousel carousel" >
+                              <ul class="slides">
+                                <li >
+                                        <img src="images/tehnologies/php.png" alt=""  />
+                                    </li>
+                                    <li >
+                                        <img src="images/tehnologies/jquery.png" alt=""  />
+                                    </li>
+                                    <li>
+                                        <img src="images/tehnologies/html5.png" alt=""  />
+                                    </li>
+                                    <li>
+                                        <img src="images/tehnologies/java.png" alt=""  />
+                                    </li>
+                                    <li>
+                                        <img src="images/tehnologies/js.png" alt="" />
+                                    </li>
+                                    <li>
+                                        <img src="images/tehnologies/mysql.png" alt=""  />
+                                    </li>
+                                    <li >
+                                        <img src="images/tehnologies/php.png" alt=""  />
+                                    </li>
+                                    <li >
+                                        <img src="images/tehnologies/jquery.png" alt="" />
+                                    </li>
+                                    <li>
+                                        <img src="images/tehnologies/html5.png" alt=""  />
+                                    </li>
+                                <!-- items mirrored twice, total of 12 -->
+                              </ul>
+                            </div>  
+                    </div>
+                    <div class="clear"></div>
+                </div>
+                </div>
+    <div class="clear">            
+</div>
+<!--/middle-->
+<?php include("footer.php"); ?>
