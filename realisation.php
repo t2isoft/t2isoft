98ee2d96de5ba817d52b0522d8215acbb3a714af
karl92390 <karl.kelleoglu@laposte.net>
2015-12-14 20:54:24 +0100

    Revert "add new preoduction"
    This reverts commit 9a5c2fc6f0326f3857348ca167cea8dec10cce1f.

--- realisation.php
+++ realisation.php
@@ -47,202 +47,149 @@
 
                         <div class="clear"></div>
                         <hr><br>
+                        <h2>En maintenance</h2>
+                       <!-- <div class="row">
+                            <div class="col col_1_3">
+                                <div class="inner">
+                                <a href="http://www.microceft.fr/">
+                                    <div class="work-item">
+                                        <div class="work-img"><img src="images/tuts/microceft.jpg" height="170px"  /></div>
+                                        <div class="work-title">
+                                            <div class="corner-orange-top"></div>
+											<h4 class="text-white text-bold">microceft.fr</h4>
+											<p class="text-white">Vente matériaux informatique</p>
+                                        </div>
+                                    </div>
+                                </a>
+
+                                </div>
+                            </div>
+
+                            <div class="col col_1_3">
+                                <div class="inner">
+                                <a href="http://cielbleu-bezons.fr/">
+                                    <div class="work-item">
+                                        <div class="work-img"><img src="images/tuts/ciel.jpg" height="170px" /></div>
+                                        <div class="work-title ">
+                                            <div class="corner-orange-top"></div>
+											<h4 class="text-white text-bold">Ciel Bleu</h4>
+											<p class="text-white">Service à la personne</p>
+                                        </div>
+                                    </div>
+                                </a>
+                                </div>
+                            </div>
+
+                            <div class="col col_1_3">
+                                <div class="inner">
+                                <a href="http://demenagement-transport-93.com/">
+                                    <div class="work-item">
+                                        <div class="work-img"><img src="images/tuts/sts.jpg" height="170px" /></div>
+                                        <div class="work-title ">
+                                            <div class="corner-orange-top"></div>
+											<h4 class="text-white text-bold">STS Transport</h4>
+											<p class="text-white">Transport & Déménagement </p>
+                                        </div>
+                                    </div>
+                                </a>
+                                </div>
+                            </div>
+                        </div>
+
                         <div class="row">
-                          <div class="col col_1_3">
-                              <div class="inner">
-                              <a href="http://www.overpoweredrecords.com/" TARGET="_blank">
-                                  <div class="work-item">
-                                      <div class="work-img"><img src="images/tuts/overpoweredrecords.png" height="170px"  /></div>
-                                      <div class="work-title">
-                                          <div class="corner-orange-top"></div>
-                                          <h4 class="text-white text-bold">Overpoweredrecords.com</h4>
-                                          <p class="text-white">Vente disques de musiques</p>
-                                      </div>
-                                  </div>
-                              </a>
-
-                              </div>
-                          </div>
-
-                          <div class="col col_1_3">
-                              <div class="inner">
-                              <a href="http://www.mannequine.890m.com/" TARGET="_blank">
-                                  <div class="work-item">
-                                      <div class="work-img"><img src="images/tuts/mannequine.png" height="170px"  /></div>
-                                      <div class="work-title">
-                                          <div class="corner-orange-top"></div>
-                                          <h4 class="text-white text-bold">Mannequine</h4>
-                                          <p class="text-white">Book d'une mannequine</p>
-                                      </div>
-                                  </div>
-                              </a>
-
-                              </div>
-                          </div>
+                            <div class="col col_1_3">
+                                <div class="inner">
+                                <a href="#">
+                                    <div class="work-item">
+                                        <div class="work-img"><img src="images/tuts/voyage.jpg" height="170px" /></div>
+                                        <div class="work-title ">
+                                            <div class="corner-orange-top"></div>
+											<h4 class="text-white text-bold">Je Voyage</h4>
+											<p class="text-white">Agence de voyage</p>
+                                        </div>
+                                    </div>
+                                </a>
+                                </div>
+                            </div>
 
                             <div class="col col_1_3">
                                 <div class="inner">
-                                <a href="http://immobilis.890m.com/" TARGET="_blank">
+                                <a href="#">
                                     <div class="work-item">
-                                        <div class="work-img"><img src="images/tuts/immobilis.png" height="170px"  /></div>
-                                        <div class="work-title">
+                                        <div class="work-img"><img src="images/tuts/shop.jpg" height="170px" /></div>
+                                        <div class="work-title ">
                                             <div class="corner-orange-top"></div>
-                      											<h4 class="text-white text-bold">Immobilis</h4>
-                      											<p class="text-white">Site d'annonces immobiliere</p>
+											<h4 class="text-white text-bold">Kaymu</h4>
+											<p class="text-white">Shopping en ligne</p>
                                         </div>
                                     </div>
                                 </a>
-
                                 </div>
                             </div>
-                            </div>
-
-                            <div class="row">
-                                <div class="col col_1_3">
-                                    <div class="inner">
-                                    <a href="http://www.jevoyage.ma/" TARGET="_blank">
-                                        <div class="work-item">
-                                            <div class="work-img"><img src="images/tuts/voyage.jpg" height="170px" /></div>
-                                            <div class="work-title ">
-                                                <div class="corner-orange-top"></div>
-                                                <h4 class="text-white text-bold">Je Voyage</h4>
-                                                <p class="text-white">Agence de voyage</p>
-                                            </div>
-                                        </div>
-                                    </a>
-                                    </div>
-                                </div>
 
                             <div class="col col_1_3">
                                 <div class="inner">
-                                <a href="http://karlkelleoglu.com/" TARGET="_blank">
+                                <a href="http://hotel-dans-paris.fr/" target="_blank">
                                     <div class="work-item">
-                                        <div class="work-img"><img src="images/tuts/karl.png" height="170px"  /></div>
-                                        <div class="work-title">
+                                        <div class="work-img"><img src="images/tuts/hotel.png" height="170px"  /></div>
+                                        <div class="work-title ">
                                             <div class="corner-orange-top"></div>
-                                            <h4 class="text-white text-bold">Karlkelleoglu.com</h4>
-                                            <p class="text-white">Portfolio professionnel</p>
+											<h4 class="text-white text-bold">Hotel dans Paris</h4>
+											<p class="text-white">Réservation en ligne</p>
                                         </div>
                                     </div>
                                 </a>
                                 </div>
-                                </div>
+                            </div>
+                        </div>
 
+                        <div class="row">
+                            <div class="col col_1_3">
+                                <div class="inner">
+                                <a href="#">
+                                    <div class="work-item">
+                                        <div class="work-img"><img src="images/tuts/iphone1.png" height="170px" width="220px" /></div>
+                                        <div class="work-title ">
+                                            <div class="corner-orange-top"></div>
+											<h4 class="text-white text-bold">Application mobile</h4>
+											<p class="text-white">Iphone, Android  </p>
+                                        </div>
+                                    </div>
+                                </a>
+                                </div>
+                            </div>
 
                             <div class="col col_1_3">
                                 <div class="inner">
-                                <a href="http://imade.890m.com/" TARGET="_blank">
+                                <a href="#">
                                     <div class="work-item">
-                                        <div class="work-img"><img src="images/tuts/imade.png" height="170px"  /></div>
+                                        <div class="work-img"><img src="images/tuts/apli.png" height="170px"   /></div>
+                                        <div class="work-title ">
+                                            <div class="corner-orange-top"></div>
+											<h4 class="text-white text-bold">Application mobile</h4>
+											<p class="text-white">Ipad, Iphone, Tablette</p>
+                                        </div>
+                                    </div>
+                                </a>
+                                </div>
+                            </div>
+
+                            <div class="col col_1_3">
+                                <div class="inner">
+                                <a href="#">
+                                    <div class="work-item">
+                                        <div class="work-img"><img src="images/tuts/appl.jpg" height="170px"  alt="" /></div>
                                         <div class="work-title">
                                             <div class="corner-orange-top"></div>
-                                            <h4 class="text-white text-bold">Imade</h4>
-                                            <p class="text-white">Garage automobile à Bezons</p>
+											<h4 class="text-white text-bold">Application mobile</h4>
+											<p class="text-white">Tous type d'application mobile</p>
                                         </div>
                                     </div>
                                 </a>
                                 </div>
-                                </div>
                             </div>
 
-                              <div class="row">
-                              <div class="col col_1_3">
-                                  <div class="inner">
-                                  <a href="http://annonceur.890m.com/" TARGET="_blank">
-                                      <div class="work-item">
-                                          <div class="work-img"><img src="images/tuts/annonceur.png" height="170px"  /></div>
-                                          <div class="work-title">
-                                              <div class="corner-orange-top"></div>
-                                              <h4 class="text-white text-bold">Annonceur</h4>
-                                              <p class="text-white">Site de petites annonces </p>
-                                          </div>
-                                      </div>
-                                  </a>
-
-                                  </div>
-                              </div>
-
-
-                              <div class="col col_1_3">
-                                  <div class="inner">
-                                  <a href="http://www.kaymu.ma/" TARGET="_blank">
-                                      <div class="work-item">
-                                          <div class="work-img"><img src="images/tuts/shop.jpg" height="170px" /></div>
-                                          <div class="work-title ">
-                                              <div class="corner-orange-top"></div>
-                                              <h4 class="text-white text-bold">Kaymu</h4>
-                                              <p class="text-white">Shopping en ligne</p>
-                                          </div>
-                                      </div>
-                                  </a>
-                                  </div>
-                              </div>
-
-                              <div class="col col_1_3">
-                                  <div class="inner">
-                                  <a href="http://hotel-dans-paris.fr/" target="_blank">
-                                      <div class="work-item">
-                                          <div class="work-img"><img src="images/tuts/hotel.png" height="170px"  /></div>
-                                          <div class="work-title ">
-                                              <div class="corner-orange-top"></div>
-                                              <h4 class="text-white text-bold">Hotel dans Paris</h4>
-                                              <p class="text-white">Réservation en ligne</p>
-                                          </div>
-                                      </div></div>
-                                  </a>
-                                  </div>
-                              </div>
-
-
-
-                            <div class="row">
-
-                              <div class="col col_1_3">
-                                  <div class="inner">
-                                  <a href="http://cielbleu-bezons.fr/" TARGET="_blank">
-                                      <div class="work-item">
-                                          <div class="work-img"><img src="images/tuts/ciel.jpg" height="170px" /></div>
-                                          <div class="work-title ">
-                                              <div class="corner-orange-top"></div>
-                                              <h4 class="text-white text-bold">Ciel Bleu</h4>
-                                              <p class="text-white">Service à la personne</p>
-                                          </div>
-                                      </div>
-                                  </a>
-                                  </div>
-                              </div>
-
-                              <div class="col col_1_3">
-                                  <div class="inner">
-                                  <a href="http://demenagement-transport-93.com/" TARGET="_blank">
-                                      <div class="work-item">
-                                          <div class="work-img"><img src="images/tuts/sts.jpg" height="170px" /></div>
-                                          <div class="work-title ">
-                                              <div class="corner-orange-top"></div>
-                                              <h4 class="text-white text-bold">STS Transport</h4>
-                                              <p class="text-white">Transport & Déménagement </p>
-                                          </div>
-                                      </div>
-                                  </a>
-                                  </div>
-                              </div>
-
-                              <div class="col col_1_3">
-                                                              <div class="inner">
-                                                              <a href="http://www.microceft.fr/">
-                                                                  <div class="work-item">
-                                                                      <div class="work-img"><img src="images/tuts/microceft.jpg" height="170px"  /></div>
-                                                                      <div class="work-title">
-                                                                          <div class="corner-orange-top"></div>
-                              											<h4 class="text-white text-bold">microceft.fr</h4>
-                              											<p class="text-white">Vente matériaux informatique</p>
-                                        </div>
-                                    </div>
-                                </a>
-
-                                </div>
-                            </div>
-
+                        </div>-->
 
                     </div>
                 </article>
