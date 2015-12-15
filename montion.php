b60695607cb70a3ee4c1fd39972160bf811b7e35
karl92390 <karl.kelleoglu@laposte.net>
2015-12-15 12:03:52 +0100

    add mentions legale

---
+++ montion.php
@@ -0,0 +1,214 @@
+<?php include("header.php"); ?>
+
+
+        <div class="black-background">
+            <div class="container">
+                 <!--header slider-->
+                    <div class="header-slider">
+                        <img src="dist/img/img/slide_img_1.jpg">
+                    </div>
+                    <!--/header slider-->
+            </div>
+        </div>
+        <div class="white-background">
+
+                <ul class="middle-menu">
+
+                    <li>
+                        <a href="team.php">
+                            <div class="tf-top-menu"></div>
+                            <img src="dist/img/ico_menu/menu1.png" alt="" />
+                            <h4 class="text-white ">Agence</h4>
+                            <div class="tf-bottom-menu"></div>
+                        </a>
+                    </li>
+
+                    <li>
+                        <a href="site-web.php" >
+                            <div class="tf-top-menu"></div>
+                            <img src="dist/img/ico_menu/menu2.png" alt="" />
+                            <h4 class="text-white">Site web</h4>
+                            <div class="tf-bottom-menu"></div>
+                        </a>
+                    </li>
+
+                    <li>
+                        <a href="application.php">
+                            <div class="tf-top-menu"></div>
+                            <img src="dist/img/ico_menu/menu3.png" alt="" />
+                            <h4 class="text-white">Applications</h4>
+                            <div class="tf-bottom-menu"></div>
+                        </a>
+                    </li>
+
+                    <li>
+                        <a href="graphisme.php">
+                            <div class="tf-top-menu"></div>
+                            <img src="dist/img/ico_menu/menu5.png" alt="" />
+                            <h4 class="text-white">Graphisme</h4>
+                            <div class="tf-bottom-menu"></div>
+                        </a>
+                    </li>
+
+                    <li>
+                        <a href="referencement.php">
+                            <div class="tf-top-menu"></div>
+                            <img src="dist/img/ico_menu/menu4.png" alt="" />
+                            <h4 class="text-white">Référencement</h4>
+                            <div class="tf-bottom-menu"></div>
+                        </a>
+                    </li>
+
+                    <li class="active-menu">
+                        <a href="maintenance.php">
+                            <div class="tf-top-menu"></div>
+                            <img src="dist/img/ico_menu/menu6.png" alt="" />
+                            <h4 class="text-white">Maintenance</h4>
+                            <div class="tf-bottom-menu"></div>
+                        </a>
+                    </li>
+
+                </ul>
+
+        </div>
+<!-- middle -->
+<div class="white-background">
+    <div class="container ">
+        <div id="middle" class="full_width box_white">
+            <!-- content -->
+            <div class="content" role="main">
+
+                <article class="post-detail">
+                    <div class="entry">
+
+                        <div class="row">
+                            <div class="col col_2_3">
+                                <div class="inner">
+                                    <div class="title">
+                                        <h1>Editeur du site</h1>
+                                    </div>
+                                    <p>T2isoft</br>
+                                      Rue gaston appert</br>
+                                      92390 Villeneuve la garenne</br></br>
+
+                                      Tel : +33 6 59 98 44 86</br>
+                                      Mail : t2isoft.dev@gmail.com</br></br></p>
+                                </div>
+                            </div>
+                        </div>
+                            <br>
+            <div class="row">
+                            <div class="col col_2_3">
+                                <div class="inner">
+                                    <div class="title">
+                                        <h1>Site hébergé par</h1>
+                                    </div>
+                                    <p>OVH</br>
+                                      SAS au capital de 500 K€</br>
+                                      RCS Roubaix – Tourcoing</p>
+                                </div>
+                            </div>
+            <div class="row">
+                            <div class="col col_2_3">
+                                <div class="inner">
+                                    <div class="title">
+                                        <h1>SIÈGE SOCIAL</h1>
+                                    </div>
+                                    <p>2 rue Kellermann</br>
+                                      59100 Roubaix</br>
+                                      Tel : 08 203 203 63</br>
+                                      SIRET : 424 761 419 00045</p>
+                                </div>
+                            </div>
+
+
+						<div class="row">
+                            <div class="col col_2_3">
+                                <div class="inner">
+                                    <div class="title">
+                                        <h1>Réalisation technique et graphique </h1>
+                                    </div>
+                                    <p>T2isoft</p>
+                                </div>
+                            </div>
+
+            <div class="row">
+                            <div class="col col_2_3">
+                                <div class="inner">
+                                    <div class="title">
+                                        <h1>Droits d’auteur</h1>
+                                    </div>
+                                    <p>L’ensemble de ce site relève de la législation française et internationale sur le droit d’auteur et la propriété intellectuelle. Les informations ainsi que les outils d’aide à la décision contenus dans ce site sont la propriété intellectuelle de T2isoft. Le contenu des informations ne peut être utilisé qu’à des fins strictement privées. La reproduction des textes est autorisée sous réserve des conditions suivantes :
+                                    </br></br>
+                                        respect de l’intégralité des documents reproduits (Sans modification ni altération)</br>
+                                        citation explicite de la (ou les) source(s) du document</br>
+                                        Exemple : « Ce document provient du site Internet de T2isoft.</br>
+                                        Les droits de reproduction sont réservés et strictement limités « , Gratuité de la diffusion.</br>
+                                        Toute autre utilisation devra faire l’objet d’une autorisation express de notre part.</br></br>
+
+                                        L’utilisation du logo de T2isoft est autorisée sous réserve des conditions suivantes :</br></br></br>
+
+                                        autorisation écrite formelle de la direction de T2isoft</br>
+                                        utilisation dans son intégralité et sans aucune modification</br>
+                                        Une autorisation tacite est donnée lorsque le logo est utilisé pour faire un lien sur la page d’accueil du site Internet www.t2isoft.com</br>
+                                        Crédit photo, tous droits réservés T2isoft</p>
+                                </div>
+                            </div>
+
+
+					</div><div class="clear"></div>
+					</div>
+                </article>
+
+        </div>
+            <!--/content-->
+
+            <div class="clear"></div>
+    </div>
+         <div class="clear"></div>
+    </div>
+                <div class="footer-slider">
+                <div class="container">
+                    <div class="slider-inner">
+                        <h2 class="text-bold">Technologies utilisées</h2>
+
+                            <div class="tf-footer-carousel carousel" >
+                              <ul class="slides">
+                                <li >
+                                        <img src="dist/img/tehnologies/php.png" alt=""  />
+                                    </li>
+                                    <li >
+                                        <img src="dist/img/tehnologies/jquery.png" alt=""  />
+                                    </li>
+                                    <li>
+                                        <img src="dist/img/tehnologies/html5.png" alt=""  />
+                                    </li>
+                                    <li>
+                                        <img src="dist/img/tehnologies/java.png" alt=""  />
+                                    </li>
+                                    <li>
+                                        <img src="dist/img/tehnologies/js.png" alt="" />
+                                    </li>
+                                    <li>
+                                        <img src="dist/img/tehnologies/mysql.png" alt=""  />
+                                    </li>
+                                    <li >
+                                        <img src="dist/img/tehnologies/php.png" alt=""  />
+                                    </li>
+                                    <li >
+                                        <img src="dist/img/tehnologies/jquery.png" alt="" />
+                                    </li>
+                                    <li>
+                                        <img src="dist/img/tehnologies/html5.png" alt=""  />
+                                    </li>
+                                <!-- items mirrored twice, total of 12 -->
+                              </ul>
+                            </div>
+                    </div>
+                    <div class="clear"></div>
+                </div>
+                </div>
+<div class="clear">
+</div>
+<!--/middle-->
+<?php include("footer.php"); ?>
